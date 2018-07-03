7e73b999f090b198aa15eb0ee7f4065a3633b233
shyam.jegadeesh <shyam.jegadeesh@servicenow.com>
2018-07-03 15:00:51 +0530

    Added comment Login

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 // Added Admin Module
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// Login Module
 import { LoginComponent } from './login/login.component';
 import { IndexComponent } from './index/index.component';
 
