c0dace083d39d9121d38f3184043915514217de6
shyam.jegadeesh <shyam.jegadeesh@servicenow.com>
2018-07-03 15:17:01 +0530

    Ng Module Change

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { IndexComponent } from './index/index.component';
 
+// Ng Module
 @NgModule({
   imports: [
     CommonModule
